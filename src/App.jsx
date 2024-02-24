c28d164bbc0ee17a93ea6fc8819fdc33f9f6c657
otsairaze <ssavelevdenchikk@gmail.com>
2024-02-24 16:24:30 +0200

    version 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import Footer from "./Components/Footer/Footer";
 import Header from "./Components/Header/Header";
 import Main from "./Pages/Main/Main";
-import React from "react";
+
 import MenuSushiSet from "./Pages/MenuSushiSet/MenuSushiSet";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-import Drawer from "./Components/Drawer/Drawer";
-import Drawermenu from "./Components/Drawer/Drawermenu";
 
 function App() {
   const Layout = ({ children }) => (
